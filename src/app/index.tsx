07dacf279ae5486e21b56a9747fe612c91be9008
Preetam Raj <iitianpreetam@gmail.com>
2025-08-30 00:35:19 +0530

    added tailwind classes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { Text, View } from 'react-native';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View className='flex-1 items-center justify-center'>
+      <Text className='text-red-500'>HomeScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
