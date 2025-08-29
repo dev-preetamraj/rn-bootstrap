98d65593fb965e86963ecdd2c78f0b4ec042b42b
Preetam Raj <iitianpreetam@gmail.com>
2025-08-30 00:31:40 +0530

    added expo router and nativewind

---
+++ src/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from 'expo-router';
+import './global.css';
+
+const RootLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name='index' options={{ headerShown: false }} />
+    </Stack>
+  );
+};
+
+export default RootLayout;
